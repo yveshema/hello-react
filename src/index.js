57b70782ad29544ec484028499270710c542ef11
Yves Shema <2939738+yveshema@users.noreply.github.com>
2023-09-11 02:12:22 +0000

    Update 2 files

--- src/index.js
+++ src/index.js
@@ -1,7 +1,11 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 
-import App from "./App";
+// import App from "./App";
+
+function App() {
+  return 'Hello world!';
+}
 
 const rootElement = document.getElementById("root");
 const root = createRoot(rootElement);
